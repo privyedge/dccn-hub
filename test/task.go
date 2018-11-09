e80eb308019ddb2814365ec5d1faa1feae5c8721
zhangyousong <zhangyousong@exutech.cn>
2018-11-08 19:36:49 -0800

    mongodb done

--- test/task.go
+++ test/task.go
@@ -9,11 +9,23 @@ import (
 func main() {
     fmt.Println("test add test:")
 
-  //  mongodb.TaskList()
+  // //  mongodb.TaskList()
+  //
+  // task := mongodb.Task{Name:"docker_image_name", Region:"us_west", Zone:"ca", Userid:1}
+  //
+  //
+  // id := mongodb.AddTask(task)
+  // fmt.Printf("new task id : %d \n", id)
 
-  task := mongodb.Task{Name:"docker_image_name", Region:"us_west", Zone:"ca", Userid:1} }
+   userid := 1
+   tasks := mongodb.TaskList(userid)
+
+   for i := range tasks {
+       task := tasks[i]
+       fmt.Printf("task id : %d \n", task.ID)
+}
 
 
-  mongodb.addTask(task)
+
 
 }
