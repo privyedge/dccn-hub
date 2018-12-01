0fc5730bd62d79fa21d5248a2d56f04b3fbcff15
Stanley Wu <35579150+fruit37@users.noreply.github.com>
2018-11-30 21:29:51 -0800

    fix format

--- taskmgr/service.go
+++ taskmgr/service.go
@@ -2,6 +2,11 @@ package taskmgr
 
 import (
 	"fmt"
+	util "github.com/Ankr-network/dccn-hub/util"
+	pb "github.com/Ankr-network/dccn-rpc/protocol"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"io"
 	"log"
 	"math/rand"
