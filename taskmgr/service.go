e039a748ca8ce157d19b46c298288d837e4a43fc
zhangyousong <zhangyousong@exutech.cn>
2018-12-11 17:29:27 -0800

    [SWDEV-109]datacenter-web-application

--- taskmgr/service.go
+++ taskmgr/service.go
@@ -31,7 +31,11 @@ type server struct {
 	dcstreams map[int]pb.Dccncli_K8TaskServer //datacenterid => stream
 }
 
-// Add a new task to a data center
+func (s *server) TaskDetail(ctx context.Context, in *pb.TaskDetailRequest) (*pb.TaskDetailResponse, error) {
+	return &pb.TaskDetailResponse{Body: ""}, nil
+
+}
+
 func (s *server) AddTask(ctx context.Context, in *pb.AddTaskRequest) (*pb.AddTaskResponse, error) {
 	fmt.Printf("received add task request\n")
 	token := in.Usertoken
@@ -42,7 +46,7 @@ func (s *server) AddTask(ctx context.Context, in *pb.AddTaskRequest) (*pb.AddTas
 		return &pb.AddTaskResponse{Status: "Failure", Taskid: -1}, nil
 	} else {
 
-		task := util.Task{Name: in.Name, Region: in.Region, Zone: in.Zone, Userid: user.ID}
+		task := util.Task{Name: in.Name, Datacenter: in.Datacenter, Userid: user.ID, Type: in.Type}
 		id := util.AddTask(task)
 		task.ID = id
 
@@ -50,6 +54,7 @@ func (s *server) AddTask(ctx context.Context, in *pb.AddTaskRequest) (*pb.AddTas
 		util.UpdateTaskUnqueName(int(id), tastName)
 
 		if in.Replica <= 0 || in.Replica > 100 {
+			fmt.Printf("replica is eror %d use default 1  \n", in.Replica)
 			in.Replica = 1
 		}
 
@@ -58,7 +63,7 @@ func (s *server) AddTask(ctx context.Context, in *pb.AddTaskRequest) (*pb.AddTas
 		stream := SelectFreeDatacenter(s)
 		if stream != nil {
 			fmt.Printf("GetTaskNameAsTaskIDForK8s  id  %d name %s \n", task.ID, task.Name)
-			var message = pb.Task{Type: "NewTask", Taskid: id, Name: tastName, Image: task.Name, Extra: "nothing"}
+			var message = pb.Task{Type: "NewTask", Taskid: id, Name: tastName, TaskType: task.Type, Image: task.Name, Extra: "nothing"}
 			//fmt.Printf("new messsage for add task %s \n", message.Name)
 			if err := stream.Send(&message); err != nil {
 				fmt.Printf(">>>send add task message %s to data center failed \n", message.Name)
@@ -134,7 +139,32 @@ func (s *server) TaskList(ctx context.Context, in *pb.TaskListRequest) (*pb.Task
 
 }
 
-// Cancel a task with given task id.
+func (s *server) DataCenterList(ctx context.Context, in *pb.DataCenterListRequest) (*pb.DataCenterListResponse, error) {
+	token := in.Usertoken
+	user := util.GetUser(token)
+	fmt.Printf("task list reqeust \n")
+
+	if user.ID == 0 {
+		fmt.Printf("task list reqeust fail for user token error\n")
+		return &pb.DataCenterListResponse{}, nil
+	} else {
+		dataCenters := util.DataCeterList()
+
+		var dcList []*pb.DataCenterInfo
+		for i := range dataCenters {
+			dataCenter := dataCenters[i]
+			dcInfo := &pb.DataCenterInfo{}
+			dcInfo.Id = dataCenter.ID
+			dcInfo.Name = dataCenter.Name
+			dcList = append(dcList, dcInfo)
+			//fmt.Printf("task id : %d %s status %s \n", task.ID,task.Name, task.Status)
+		}
+
+		return &pb.DataCenterListResponse{DcList: dcList}, nil
+	}
+
+}
+
 func (s *server) CancelTask(ctx context.Context, in *pb.CancelTaskRequest) (*pb.CancelTaskResponse, error) {
 	fmt.Printf("received cancel task request\n")
 	token := in.Usertoken
@@ -220,6 +250,7 @@ func (s *server) UpdateTask(ctx context.Context, in *pb.UpdateTaskRequest) (*pb.
 		}
 
 		if in.Replica <= 0 || in.Replica > 100 {
+			fmt.Printf("replica is eror %d \n", in.Replica)
 			in.Replica = int64(task.Replica)
 		}
 
@@ -233,6 +264,15 @@ func (s *server) UpdateTask(ctx context.Context, in *pb.UpdateTaskRequest) (*pb.
 		util.UpdateTaskImage(int(in.Taskid), in.Name)
 		util.UpdateTask(int(in.Taskid), "updating", 0)
 
+		// if they are same use 0 as default value
+		if int(in.Replica) == task.Replica {
+			in.Replica = 0
+		}
+
+		if in.Name == task.Name {
+			in.Name = ""
+		}
+
 		if sendMessageToK8(datacenter, "UpdateTask", in.Taskid, task.Uniquename, in.Name, int(in.Replica), "") == false {
 			delete(s.dcstreams, int(task.Datacenterid))
 		}
@@ -245,11 +285,11 @@ func (s *server) K8ReportStatus(ctx context.Context, in *pb.ReportRequest) (*pb.
 	fmt.Printf("received K8ReportStatus request %s\n", in.Report)
 	datacenter := util.GetDataCenter(in.Name)
 	if datacenter.ID == 0 {
-		datacenter := util.DataCenter{Name: in.Name, Report: in.Report, Host: in.Host, Port: in.Port}
+		datacenter := util.DataCenter{Name: in.Name, Report: in.Report}
 		id := util.AddDataCenter(datacenter)
 		fmt.Printf("insert new  DataCenter id %d \n", id)
 	} else {
-		datacenter2 := util.DataCenter{Name: in.Name, Report: in.Report, Host: in.Host, Port: in.Port}
+		datacenter2 := util.DataCenter{Name: in.Name, Report: in.Report}
 		util.UpdateDataCenter(datacenter2, int(datacenter.ID))
 		fmt.Printf("update  DataCenter id %d \n", datacenter.ID)
 
@@ -280,7 +320,7 @@ func (s *server) K8Task(stream pb.Dccncli_K8TaskServer) error {
 			taskId := util.GetTaskIDFromTaskNameForK8s(in.Taskname)
 			fmt.Printf("<<<received task  id : %d  status: %s  datacenter : %s \n", taskId, in.Status, in.Datacenter)
 
-			processTaskStatus(taskId, in.Status, in.Datacenter)
+			processTaskStatus(taskId, in.Status, in.Datacenter, in.Url)
 		}
 
 		s.mu.Unlock()
@@ -307,8 +347,7 @@ func updateDataCenter(s *server, in *pb.K8SMessage, stream pb.Dccncli_K8TaskServ
 
 }
 
-//deal with Task message from DataCenter
-func processTaskStatus(taskid int64, status string, dcName string) {
+func processTaskStatus(taskid int64, status string, dcName string, url string) {
 	datacenter := util.GetDataCenter(dcName)
 	// fmt.Printf the error message for now. They are supposed to be throw as an exception in the future.
 	if datacenter.ID == 0 {
@@ -319,6 +358,9 @@ func processTaskStatus(taskid int64, status string, dcName string) {
 
 		fmt.Printf("processTaskStatus %d %s\n", taskid, status)
 		if status == "StartSuccess" {
+			if len(url) > 0 {
+				util.UpdateTaskURL(int(taskid), url)
+			}
 			util.UpdateTask(int(taskid), "running", int(datacenter.ID))
 		}
 
--- util/mongo.go
+++ util/mongo.go
@@ -20,8 +20,8 @@ type Task struct {
 	ID           int64
 	Userid       int64
 	Name         string
-	Region       string
-	Zone         string
+	Datacenter   string
+	Type         string
 	Replica      int
 	Datacenterid int64  // mongodb name is low field
 	Status       string // 1 new 2 running 3. done 4 cancelling 5.canceled 6. updating 7. updateFailed
@@ -45,8 +45,6 @@ type DataCenter struct {
 	ID             int64
 	Name           string
 	Report         string
-	Host           string
-	Port           int64
 	LastReportTime int64
 	Status         string //1. running  2. stopped  3. dropped
 	DatacenterId   int
@@ -68,7 +66,7 @@ func AddDataCenter(d DataCenter) int64 {
 	// fmt.Printf("Id of person: %d\n", p._id)
 	id := GetID("datacenterid", db)
 	msec := time.Now().UnixNano() / 1000000
-	err := c.Insert(bson.M{"_id": id, "id": id, "name": d.Name, "report": d.Report, "host": d.Host, "port": d.Port, "lastReporTtime": msec, "status": "Running"})
+	err := c.Insert(bson.M{"_id": id, "id": id, "name": d.Name, "report": d.Report, "lastReporTtime": msec, "status": "Running"})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -80,7 +78,7 @@ func UpdateDataCenter(d DataCenter, id int) {
 	c := db.C("datacenter")
 	fmt.Printf("UpdateDataCenter report %s \n", d.Report)
 
-	c.Update(bson.M{"_id": id}, bson.M{"$set": bson.M{"name": d.Name, "report": d.Report, "host": d.Host, "port": d.Port}})
+	c.Update(bson.M{"_id": id}, bson.M{"$set": bson.M{"name": d.Name, "report": d.Report}})
 }
 
 var instance *mgo.Database
@@ -111,11 +109,8 @@ func mongodbconnect() *mgo.Database {
 func AddTask(task Task) int64 {
 	db := GetDBInstance()
 	c := db.C("task")
-	//p := Person{"xxxx", "123455"}
-	// p._id = 19
-	// fmt.Printf("Id of person: %d\n", p._id)
 	id := GetID("taskid", db)
-	err := c.Insert(bson.M{"_id": id, "id": id, "name": task.Name, "userid": task.Userid, "region": task.Region, "zone": task.Zone, "status": "new"})
+	err := c.Insert(bson.M{"_id": id, "id": id, "name": task.Name, "userid": task.Userid, "type": task.Type, "datacenter": task.Datacenter, "status": "new"})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -149,6 +144,18 @@ func TaskList(userid int) []Task {
 	return tasks
 }
 
+func DataCeterList() []DataCenter {
+	var dataCenters []DataCenter
+	db := GetDBInstance()
+	c := db.C("datacenter")
+	result := DataCenter{}
+	iter := c.Find(nil).Limit(100).Iter()
+	for iter.Next(&result) {
+		dataCenters = append(dataCenters, result)
+	}
+	return dataCenters
+}
+
 func GetTask(taskid int) Task {
 	task := Task{}
 	db := GetDBInstance()
@@ -175,11 +182,10 @@ func UpdateTask(taskid int, status string, datacentrid int) {
 	}
 }
 
-
 func UpdateTaskReplica(taskid int, replica int) {
 	db := GetDBInstance()
 	c := db.C("task")
-    c.Update(bson.M{"_id": taskid}, bson.M{"$set": bson.M{"replica": replica}})
+	c.Update(bson.M{"_id": taskid}, bson.M{"$set": bson.M{"replica": replica}})
 }
 
 func UpdateTaskUnqueName(taskid int, uniqueName string) {
@@ -192,6 +198,12 @@ func UpdateTaskImage(taskid int, image string) {
 	db := GetDBInstance()
 	c := db.C("task")
 	c.Update(bson.M{"_id": taskid}, bson.M{"$set": bson.M{"name": image}})
+}
+
+func UpdateTaskURL(taskid int, url string) {
+	db := GetDBInstance()
+	c := db.C("task")
+	c.Update(bson.M{"_id": taskid}, bson.M{"$set": bson.M{"url": url}})
 }
 
 func CancelTask(taskid int) {
