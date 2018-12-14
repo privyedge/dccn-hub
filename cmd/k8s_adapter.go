ee5294f0d5e25fd86e4a78df4a3a77ecd904d6ae
zhangyousong <zhangyousong@exutech.cn>
2018-12-13 20:44:32 -0800

    [SWDEV-109]decompose-to-three

---
+++ cmd/k8s_adapter.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Ankr-network/dccn-hub/k8s_adapter"
+
+func main() {
+	k8s_adapter.StartService()
+}
---
+++ util/constants.go
@@ -0,0 +1,8 @@
+package util
+
+const TaskManagerQueueName = "task_manager"
+const NewTaskEvent = "New"
+const UpdateTaskEvent = "Update"
+const CancelTaskEvent = "Cancel"
+
+const DataCenterName = "datacenter_1"
