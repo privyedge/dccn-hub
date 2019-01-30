a6a24ecba70df39e3a2dd2f92e77f1ad75b599f7
ticker <994336359@qq.com>
2019-01-30 18:58:45 +0800

    SWDEV-135 optimize return information.

---
+++ vendor/github.com/micro/go-plugins/broker/rabbitmq/options.go
@@ -0,0 +1,64 @@
+package rabbitmq
+
+import (
+	"context"
+
+	"github.com/micro/go-micro/broker"
+)
+
+type durableQueueKey struct{}
+type headersKey struct{}
+type prefetchCountKey struct{}
+type prefetchGlobalKey struct{}
+type exchangeKey struct{}
+type requeueOnErrorKey struct{}
+type deliveryMode struct{}
+
+// DurableQueue creates a durable queue when subscribing.
+func DurableQueue() broker.SubscribeOption {
+	return setSubscribeOption(durableQueueKey{}, true)
+}
+
+// Headers adds headers used by the headers exchange
+func Headers(h map[string]interface{}) broker.SubscribeOption {
+	return setSubscribeOption(headersKey{}, h)
+}
+
+// RequeueOnError calls Nack(muliple:false, requeue:true) on amqp delivery when handler returns error
+func RequeueOnError() broker.SubscribeOption {
+	return setSubscribeOption(requeueOnErrorKey{}, true)
+}
+
+// Exchange is an option to set the Exchange
+func Exchange(e string) broker.Option {
+	return setBrokerOption(exchangeKey{}, e)
+}
+
+// PrefetchCount ...
+func PrefetchCount(c int) broker.Option {
+	return setBrokerOption(prefetchCountKey{}, c)
+}
+
+// PrefetchGlobal creates a durable queue when subscribing.
+func PrefetchGlobal() broker.Option {
+	return setBrokerOption(prefetchGlobalKey{}, true)
+}
+
+// DeliveryMode sets a delivery mode for publishing
+func DeliveryMode(value uint8) broker.PublishOption {
+	return setPublishOption(deliveryMode{}, value)
+}
+
+type subscribeContextKey struct{}
+
+// SubscribeContext set the context for broker.SubscribeOption
+func SubscribeContext(ctx context.Context) broker.SubscribeOption {
+	return setSubscribeOption(subscribeContextKey{}, ctx)
+}
+
+type ackSuccessKey struct{}
+
+// AckOnSuccess will automatically acknowledge messages when no error is returned
+func AckOnSuccess() broker.SubscribeOption {
+	return setSubscribeOption(ackSuccessKey{}, true)
+}
