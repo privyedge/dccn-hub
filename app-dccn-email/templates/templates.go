3d675ecfd77364e6fa375450b3ab9be9d054bb97
Stanley Wu <35579150+fruit37@users.noreply.github.com>
2019-02-24 23:47:00 -0800

    adding more email

--- app-dccn-email/templates/templates.go
+++ app-dccn-email/templates/templates.go
@@ -19,3 +19,59 @@ const RegistrationTemplate = `
 	<a href="mailto:account@ankr.network?subject=Account%20Activation%20issue">Ankr Network Account Support</a>.
 </p>
 `
+
+// ForgotPasswordTemplate provides the email template when a user forgets his/her
+// password and choose to reset
+const ForgotPasswordTemplate = `
+<p>
+	<b>Hello Ankr User</b>,
+</p>
+<p>
+	You are receiving this message because you or somebody else has attempted to reset 
+	your password on Ankr Portal. If you did not request a new password, please disregard this message.
+</p>
+<p>
+	To reset your password, please click here: 
+	<a href="https://{{.AppDomain}}/reset_verify?code={{.Code}}&email={{.Email}}">Reset Password</a>
+</p>
+<p>
+	If clicking the link above doesn't work, please copy and paste the below URL in a 
+	new browser window.<br />
+	https://{{.AppDomain}}/reset_verify?code={{.Code}}&email={{.Email}}
+</p>
+<p>
+	Thanks,<br />
+	The Ankr Team
+</p>
+`
+
+// ChangeEmailTemplate provides the email template when a user choose to
+// change his/her email
+const ChangeEmailTemplate = `
+<p>
+	<b>Hello Ankr User</b>,
+</p>
+<p>
+	You are receiving this message because you requested a change to the email address on your Ankr account to {{.NewEmail}}.
+</p>
+<p>
+	To confirm this change, please click <a href="https://{{.AppDomain}}/email_verify?new_email={{.NewEmailEncoded}}">here</a>. 
+	Note that this link will expire in 24 hours.
+</p>
+<p>
+	If clicking the link above doesn't work, please copy and paste the below URL in a new browser window.<br />
+	https://{{.AppDomain}}/email_verify?new_email={{.NewEmailEncoded}}</p>
+<p>
+	Thanks,<br /> 
+	The Ankr Team
+</p>
+<p>
+	If you did not authorize this change, please contact 
+	<a href="mailto:account@ankr.network?subject=Change%20Email%20issue">Ankr Network Account Support</a>.
+</p>
+<hr />
+<p>
+	You are receiving this email because you took an action on 
+	<a href="https://www.ankr.com/">Ankr.com</a>.<br />
+</p>
+`
